a7373ccdadcda3af260cf3f92d5358d9c253fcc4
baps <baps@bapss-MacBook-Pro.local>
2019-01-28 18:23:24 +0530

    initial code

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow, dialog, Menu } = require('electron')
+
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }));
+   win.maximize();
+}
+
+app.on('ready', createWindow);
