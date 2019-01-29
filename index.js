8fa0fd93cd1120b538d895bd15cd87e859af7f9f
baps <baps@bapss-MacBook-Pro.local>
2019-01-29 18:07:02 +0530

    added package scripts

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+const url = require('url')
+const path = require('path')
+
+let win;
+let trayIcon = null;
+trayIcon = path.join(__dirname, 'assets/gmail.png')
+
+function createWindow() {
+   win = new BrowserWindow(
+      {
+         width: 800, 
+         height: 600, 
+         title: 'Akeo-GSuit',
+         icon: trayIcon,
+         toolbar: true,
+         webPreferences: {
+            nativeWindowOpen: true
+         },
+         icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+      }
+   )
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }));
+   win.maximize();
+
+}
+
+app.on('ready', createWindow);
